cc1c33965cc882350c717356b9fe80eb8c7a1b23
hfidelis <heitorc88@gmail.com>
2023-12-21 21:06:10 -0300

    fix: added loading and correct view for empty posts arr

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { faChevronDown, faMagnifyingGlass, faSquarePlus, IconDefinition } from '@fortawesome/free-solid-svg-icons';
-import { Observable } from 'rxjs';
+import { Observable, delay } from 'rxjs';
 import { Post } from 'src/app/models/Post.model';
 import { PostService } from 'src/app/services/post/post.service';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth/auth.service';
-import { AuthGuardService } from 'src/app/services/auth/auth-guard.service';
 
 @Component({
   selector: 'app-home',
